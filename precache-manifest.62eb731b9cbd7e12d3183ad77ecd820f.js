b7532db85136016715c9b72d3266c4a0a926015b
Studio Fomalhaut <studio@fomalhaut.nl>
2019-06-16 13:31:22 +0100

    commit goggles

---
+++ precache-manifest.62eb731b9cbd7e12d3183ad77ecd820f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdb44345e3e66e3bbb82",
+    "url": "/static/css/main.439da8ee.chunk.css"
+  },
+  {
+    "revision": "cdb44345e3e66e3bbb82",
+    "url": "/static/js/main.cdb44345.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ff73009c1a81b3437e00",
+    "url": "/static/js/2.ff73009c.chunk.js"
+  },
+  {
+    "revision": "b198ce6394bc963a3f892463ea6fd679",
+    "url": "/static/media/goggles.b198ce63.png"
+  },
+  {
+    "revision": "810f8614c5b5d79aa5a21596908877e4",
+    "url": "/index.html"
+  }
+];
